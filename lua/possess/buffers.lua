80fd51c5739fea3ea14908ec5cee57744f3d4dfa
aignemb <aigne.mb@gmail.com>
2026-08-21 07:00:37 -0400

    added terminal buffer

--- lua/possess/buffers.lua
+++ lua/possess/buffers.lua
@@ -9,6 +9,9 @@ M.buf1 = {
 		print("hello from buf1")
 	end,
 }
+function M.init_buf1()
+	possess.init_buf(M.buf1)
+end
 
 M.buf2 = {
 	name = "buf2",
@@ -17,5 +20,20 @@ M.buf2 = {
 		print("hello from buf2")
 	end,
 }
+function M.init_buf2()
+	possess.init_buf(M.buf2)
+end
+
+M.terminal = {
+	name = "terminal",
+	window = "float",
+	fn = function(buf)
+		vim.bo[buf].buftype = "terminal"
+		vim.api.nvim_open_term(buf, { cmd = { vim.o.shell } })
+	end,
+}
+function M.init_terminal()
+	possess.init_buf(M.terminal)
+end
 
 return M
